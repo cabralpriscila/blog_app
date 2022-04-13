60ce193601170ab3e65f586f81a85fc9132f810a
priscila cabral <priscilacabral.dev@gmail.com>
2022-04-12 23:12:58 -0300

    :tada: the first signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save.
+    if @user.save    
+      flash[:success] = "Welcome to the Blog App!"  
+      redirect_to @user
     else
       render 'new'
     end
