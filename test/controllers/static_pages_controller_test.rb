3e588364a068b3e2fa3d63f718c52935a620ad14
priscila cabral <priscilacabral.dev@gmail.com>
2022-04-12 21:40:42 -0300

    :recycle: sample app renamed to blog app

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Ruby on Rails Tutorial Blog App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Blog App"
   end
 
   test "should get help" do
